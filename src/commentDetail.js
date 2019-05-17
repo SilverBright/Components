a79f5837a73100a2f9a9f09cac793391d1e3babf
github username <github email address>
2019-05-17 16:22:46 -0700

    pass props to child component

--- src/commentDetail.js
+++ src/commentDetail.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import faker from 'faker';
 
-const commentDetail = () => {
+// This is a child component
+
+const CommentDetail = props => {
   return (
     <div className="comment">
         <a href="/" className="avatar">
@@ -8,44 +11,17 @@ const commentDetail = () => {
         </a>
         <div className="content">
           <a href="/" className="author">
-            Babs
+            {/* Babs */}
+            {props.author}
           </a>
           <div className="metadata">
             <span className="date">Today at 6:00pm</span>
           </div>
           <div className="text">Nice blog post!
-          </div>
         </div>
       </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Babs
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!
-          </div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Babs
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!
-          </div>
-        </div>
-      </div>
-  )
+    </div>
+  );
 }
+
+export default CommentDetail; 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+// This is a parent component
 
 const App = () => {
   return (
     <div className="ui container comments">
-      
+      {/* CommmentDetail prop="value" */}
+      <CommentDetail author="Babs"/>
+      <CommentDetail author="Pat"/>
+      <CommentDetail author="Fred"/>
     </div>
   );
 };
